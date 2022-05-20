6d322d708db7e4a206a14c13105299887ad113a3
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-20 20:43:36 +0100

    playing with try catch

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -1,13 +1,19 @@
 import clientPromise from '@/lib/mongodb'
 
-const handler = async (req) => {
-  const dbConnection = await clientPromise;
-  const db = dbConnection.db('wc');
-  return db.collection("goal").insertOne(req.body, (err, result) => {
-    if (err) return console.log(err);
-    return result
-  })
-  
+const handler = async (req, res) => {
+  let dbConnection;
+  try {
+    dbConnection = await clientPromise;
+    const db = dbConnection.db('wc');
+    const goal = await db.collection("goal").insertOne(req.body);
+    return res.json({status: 200, data: goal})
+  }
+  catch(err) {
+    return err
+  }
+  finally {
+    dbConnection.close()
+  }
 }
 
 export default handler;
