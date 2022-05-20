12a09ca8e051faa16c441f7420ba7cdc37725ae6
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-20 21:29:37 +0100

    mongo

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -1,23 +1,19 @@
 import clientPromise from '@/lib/mongodb'
 
 const handler = async (req, res) => {
-  console.log("poo")
-  return res.json({status:200, data: req.body})
-  // let dbConnection;
-  // try {
-  //   console.log(req.body)
-  //   dbConnection = await clientPromise;
-  //   const db = dbConnection.db('wc');
-  //   const goal = await db.collection("goal").insertOne(req.body);
-  //   console.log("goal:", goal)
-  //   res.json({status: 200, data: goal})
-  // }
-  // catch(err) {
-  //   return console.log("err:", err)
-  // }
-  // finally {
-  //   dbConnection.close()
-  // }
+  let dbConnection;
+  try {
+    dbConnection = await clientPromise;
+    const db = dbConnection.db('wc');
+    const goal = await db.collection("goal").insertOne(req.body);
+    res.json({ status: 200, data: goal })
+  }
+  catch(err) {
+    console.log("err:", err)
+  }
+  finally {
+    dbConnection.close()
+  }
 }
 
 export default handler;
