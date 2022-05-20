b328f2a6aba13152e155a8e2d8c0d3eedc3ca702
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-20 18:48:00 +0100

    init

---
+++ pages/api/goals.js
@@ -0,0 +1,15 @@
+import clientPromise from '@/lib/mongodb'
+
+const handler = async (req, res) => {
+  // Get data from your database
+  const dbConnection = await clientPromise;
+  const db = dbConnection.db();
+  db.collection("goal").insertOne(req.body, (err, res) => {
+    if(err) return console.log(err);
+    // response.json(res)
+    console.log("poo")
+  })
+  res.json(res)
+}
+
+export default handler;
