798821c42cbae647c2b5417c00bebf118bfdf352
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 07:06:47 +0100

    playing with mongo

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -1,31 +1,11 @@
-// import clientPromise from '@/lib/mongodb'
-
-// const handler = async (req, res) => {
-//   let dbConnection;
-//   let returnedValue;
-//   try {
-//     dbConnection = await clientPromise;
-//     const db = dbConnection.db('wc');
-//     const goal = await db.collection("goal").insertOne(req.body);
-//     res.json({ status: 200, data: goal })
-//   }
-//   catch(err) {
-//     console.log("err:", err)
-//   }
-//   finally {
-//     dbConnection.close()
-//   }
-// }
-
-// export default handler;
-
-
 import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
+  console.log(1)
   const db = await connectToDatabase();
   // Insert the event object, which is the test data we pass in
   const result = await db.collection("goals").insertOne(req.body);
+  console.log(2)
   const response = {
     statusCode: 200,
     body: JSON.stringify(result),
