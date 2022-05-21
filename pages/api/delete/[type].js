bcf03293c9adb84c355921a48646ece2ccfd3d2a
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 13:48:54 +0100

    updating deletion

--- pages/api/delete/[type].js
+++ pages/api/delete/[type].js
@@ -4,12 +4,12 @@ const handler = async (req, res) => {
   const db = await connectToDatabase();
   const { _id } = req.body;
   const { type } = req.query
-  
+
   // Insert the event object, which is the test data we pass in
   await db.collection(type).remove({"_id": _id});
   const response = {
     statusCode: 200,
-    data: JSON.stringify(goal)
+    data: `Successfully deleted ${type}`
   };
   res.json(response);
 };
