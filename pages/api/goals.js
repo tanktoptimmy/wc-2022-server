6331a5745e7b1823b6f238fdfffc5aa53379dfb1
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 07:36:25 +0100

    changing goals

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -3,7 +3,7 @@ import connectToDatabase from '@/lib/mongodb';
 const handler = async (req, res) => {
   console.log(323)
   const db = await connectToDatabase();
-  const goal = JSON.parse(req.body);
+  const goal = req.body;
   // Insert the event object, which is the test data we pass in
   await db.collection("goals").insertOne(goal);
   console.log(2)
