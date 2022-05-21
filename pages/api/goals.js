6dc2f4c29e13227cfa28c5df5b28c7c9b4278b2c
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 07:34:16 +0100

    changing goals

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -1,23 +1,16 @@
-import clientPromise from '@/lib/mongodb'
+import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
-  console.log("poo")
-  res.json({status:200, data:"poo"})
-  // let dbConnection;
-  // try {
-  //   console.log(req.body)
-  //   dbConnection = await clientPromise;
-  //   const db = dbConnection.db('wc');
-  //   const goal = await db.collection("goal").insertOne(req.body);
-  //   console.log("goal:", goal)
-  //   res.json({status: 200, data: goal})
-  // }
-  // catch(err) {
-  //   return console.log("err:", err)
-  // }
-  // finally {
-  //   dbConnection.close()
-  // }
-}
+  console.log(323)
+  const db = await connectToDatabase();
+  const goal = JSON.parse(req.body);
+  // Insert the event object, which is the test data we pass in
+  await db.collection("goals").insertOne(goal);
+  console.log(2)
+  const response = {
+    statusCode: 200
+  };
+  res.json(response);
+};
 
 export default handler;
