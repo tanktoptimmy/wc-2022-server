57e9d59d956c1c9502e00101248de5f7c19a421b
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 13:40:35 +0100

    changing collection to goal

--- pages/api/goal.js
+++ pages/api/goal.js
@@ -3,9 +3,8 @@ import connectToDatabase from '@/lib/mongodb';
 const handler = async (req, res) => {
   const db = await connectToDatabase();
   const goal = req.body;
-  console.log(goal)
   // Insert the event object, which is the test data we pass in
-  await db.collection("goals").replaceOne({"_id": goal._id}, goal, {upsert: true});
+  await db.collection("goal").replaceOne({"_id": goal._id}, goal, {upsert: true});
   const response = {
     statusCode: 200,
     data: JSON.stringify(goal)
