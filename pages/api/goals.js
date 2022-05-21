96bad855d410e4118c03963cb8ca77c215405cc4
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 07:39:16 +0100

    sending data back

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -1,14 +1,13 @@
 import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
-  console.log(323)
   const db = await connectToDatabase();
   const goal = req.body;
   // Insert the event object, which is the test data we pass in
   await db.collection("goals").insertOne(goal);
-  console.log(2)
   const response = {
-    statusCode: 200
+    statusCode: 200,
+    data: JSON.stringify(goal)
   };
   res.json(response);
 };
