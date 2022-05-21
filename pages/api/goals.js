6b0440dbf0e990937333eb625786486cb385338f
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-21 07:14:35 +0100

    removing crap

--- pages/api/goals.js
+++ pages/api/goals.js
@@ -1,14 +1,14 @@
-import connectToDatabase from '@/lib/mongodb';
+// import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
-  console.log(1)
-  const db = await connectToDatabase();
-  // Insert the event object, which is the test data we pass in
-  const result = await db.collection("goals").insertOne(req.body);
-  console.log(2)
+  // console.log(1)
+  // const db = await connectToDatabase();
+  // // Insert the event object, which is the test data we pass in
+  // const result = await db.collection("goals").insertOne(req.body);
+  // console.log(2)
   const response = {
     statusCode: 200,
-    body: JSON.stringify(result),
+    body: "poo"//JSON.stringify(result),
   };
   return response;
 };
