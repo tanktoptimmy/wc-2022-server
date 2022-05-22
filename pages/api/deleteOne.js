12a8a3df6e8e383a7ae7117ceb27cf2ae2444e3a
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-22 14:23:32 +0100

    del

--- pages/api/deleteOne.js
+++ pages/api/deleteOne.js
@@ -1,15 +1,17 @@
-import connectToDatabase from '@/lib/mongodb';
+// import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
   console.log("delete handler called")
-  const db = await connectToDatabase();
-  const { _id, _type } = req.body
-  const result = await db.collection(_type).deleteOne({_id});
-  const response = {
-    statusCode: 200,
-    data: JSON.stringify(result)
-  };
-  res.json(response);
+  // const db = await connectToDatabase();
+  // const { _id, _type } = req.body
+  // const result = await db.collection(_type).deleteOne({_id});
+  // const response = {
+  //   statusCode: 200,
+  //   data: JSON.stringify(result)
+  // };
+  // res.json(response);
+  console.log("ok we're there", req.body)
+  res.json({status: 200})
 };
 
 export default handler;
