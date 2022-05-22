94a06438e1b587b4768332e66c2353fd5f9206ae
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-22 13:58:13 +0100

    changing names

--- pages/api/deleteOne.js
+++ pages/api/deleteOne.js
@@ -3,10 +3,8 @@ import connectToDatabase from '@/lib/mongodb';
 const handler = async (req, res) => {
   console.log("delete handler called")
   const db = await connectToDatabase();
-  const { _id } = req.body
-  console.log("_id:", _id);
-  const { type } = req.query
-  const result = await db.collection(type).deleteOne({_id});
+  const { _id, _type } = req.body
+  const result = await db.collection(_type).deleteOne({_id});
   const response = {
     statusCode: 200,
     data: JSON.stringify(result)
--- pages/api/replaceOne.js
+++ pages/api/replaceOne.js
@@ -3,7 +3,7 @@ import connectToDatabase from '@/lib/mongodb';
 const handler = async (req, res) => {
   const db = await connectToDatabase();
   const body = req.body;
-  const {_id, type} = body;
+  const {_id, _type} = body;
   // Insert the event object, which is the test data we pass in
   await db.collection(_type).replaceOne({"_id": _id}, body, {upsert: true});
   const response = {
