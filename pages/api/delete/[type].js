beff9ff76b144968c7ed1ec172defc4ad579f3c0
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-22 13:16:49 +0100

    helping consoles

--- pages/api/delete/[type].js
+++ pages/api/delete/[type].js
@@ -1,9 +1,10 @@
-import { ObjectId } from 'mongodb';
 import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
+  console.log("delete handler called")
   const db = await connectToDatabase();
   const { _id } = JSON.parse(req.body)
+  console.log("_id:", _id);
   const { type } = req.query
   const result = await db.collection(type).deleteOne({_id});
   const response = {
