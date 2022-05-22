8c2dbb4e167af0055de37fd771c6e191db85e3b4
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-22 14:51:04 +0100

    playing some more

--- pages/api/deleteOne.js
+++ pages/api/deleteOne.js
@@ -1,17 +1,13 @@
 import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
-  console.log("delete handler called")
   const db = await connectToDatabase();
-  console.log(req.body)
   const { _id, _type } = req.body
   const result = await db.collection(_type).deleteOne({_id});
   const response = {
     statusCode: 200,
     data: JSON.stringify(result)
   };
-
-  console.log("unpublished response:", response)
   res.json(response);
 };
 
--- pages/api/replaceOne.js
+++ pages/api/replaceOne.js
@@ -4,8 +4,7 @@ const handler = async (req, res) => {
   const db = await connectToDatabase();
   const body = req.body;
   const {_id, _type} = body;
-  // Insert the event object, which is the test data we pass in
-  await db.collection(_type).replaceOne({"_id": _id}, body, {upsert: true});
+  await db.collection(_type).replaceOne({_id}, body, {upsert: true});
   const response = {
     statusCode: 200,
     data: JSON.stringify(body)
