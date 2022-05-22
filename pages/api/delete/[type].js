afce986bf85dab8421fc2c3c0a106e245cdad3ef
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-22 13:03:58 +0100

    updating deletion

--- pages/api/delete/[type].js
+++ pages/api/delete/[type].js
@@ -1,15 +1,14 @@
+import { ObjectId } from 'mongodb';
 import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
   const db = await connectToDatabase();
-  const { _id } = req.body;
+  const { _id } = JSON.parse(req.body)
   const { type } = req.query
-
-  // Insert the event object, which is the test data we pass in
-  await db.collection(type).remove({"_id": _id});
+  const result = await db.collection(type).deleteOne({_id});
   const response = {
     statusCode: 200,
-    data: `Successfully deleted ${type}`
+    data: JSON.stringify(result)
   };
   res.json(response);
 };
