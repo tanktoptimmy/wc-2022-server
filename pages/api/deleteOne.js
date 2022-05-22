6f5ec29c29555bf2d8d45c709b8ec1bce4d74a8a
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-05-22 14:26:51 +0100

    poop

--- pages/api/deleteOne.js
+++ pages/api/deleteOne.js
@@ -1,7 +1,7 @@
 // import connectToDatabase from '@/lib/mongodb';
 
 const handler = async (req, res) => {
-  console.log("delete handler called")
+  console.log("delete handler called?")
   // const db = await connectToDatabase();
   // const { _id, _type } = req.body
   // const result = await db.collection(_type).deleteOne({_id});
